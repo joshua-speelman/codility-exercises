79a53bc4681a93b022df111b0b80d61dfc35be63
Joshua Speelman <joshua.m.speelman@gmail.com>
2025-09-30 20:13:57 +1000

    more efficient solution. hashmapping

--- 2.2oddoccurrencesinarray.js
+++ 2.2oddoccurrencesinarray.js
@@ -1,46 +1,73 @@
+// function solution(A) {
+//     const N = A.length;
+//     const minAllowedValueForN = 1;
+//     const maxAllowedValueForN = 1000000;
+//     const pairedValues = [];
+//     let oddOneOut = null;
+
+//     if (!Array.isArray(A)) {
+//         console.error(`Error: Input A must be an array. Received: ${A}`);
+//         return [];
+//     }
+
+//     if (N < minAllowedValueForN || N > maxAllowedValueForN) {
+//         console.error(
+//             `${N} must be between ${minAllowedValueForN} and ${maxAllowedValueForN}.`
+//         );
+//         return null;
+//     }
+
+//     for (let i = A.length - 1; i >= 0; i--) {
+//         const currentNum = A[i];
+
+//         for (let j = i - 1; j >= 0; j--) {
+//             const potentialMatch = A[j];
+
+//             if (currentNum === potentialMatch) {
+//                 pairedValues.push(currentNum, potentialMatch);
+
+//                 A.splice(i, 1);
+//                 A.splice(j, 1);
+//                 i = A.length;
+//                 break;
+//             }
+//         }
+//     }
+
+//     if (A.length === 1) {
+//         oddOneOut = A[0];
+//     } else if (A.length > 1) {
+//         console.error("Error: More than one unpaired value found in array.");
+//     }
+
+//     return oddOneOut;
+// }
+
+// let testArr = [9, 3, 9, 3, 9, 7, 9];
+// console.log("Original array:", testArr);
+// let result = solution(testArr);
+// console.log("Odd one out:", result);
+
 function solution(A) {
-    const N = A.length;
-    const minAllowedValueForN = 1;
-    const maxAllowedValueForN = 1000000;
-    const pairedValues = [];
-    let oddOneOut = null;
+    let counts = {};
 
-    if (!Array.isArray(A)) {
-        console.error(`Error: Input A must be an array. Received: ${A}`);
-        return [];
+    for (const num of A) {
+        if (counts[num]) {
+            counts[num]++;
+        } else {
+            counts[num] = 1;
+        }
+        console.log(`counts after processing ${num}:`, counts);
     }
 
-    if (N < minAllowedValueForN || N > maxAllowedValueForN) {
-        console.error(
-            `${N} must be between ${minAllowedValueForN} and ${maxAllowedValueForN}.`
-        );
-        return null;
-    }
+    console.log("Final counts:", counts);
 
-    for (let i = A.length - 1; i >= 0; i--) {
-        const currentNum = A[i];
-
-        for (let j = i - 1; j >= 0; j--) {
-            const potentialMatch = A[j];
-
-            if (currentNum === potentialMatch) {
-                pairedValues.push(currentNum, potentialMatch);
-
-                A.splice(i, 1);
-                A.splice(j, 1);
-                i = A.length;
-                break;
-            }
+    for (const numKey in counts) {
+        if (counts[numKey] % 2 !== 0 ) {
+            console.log(`Odd occurrence found: ${numKey}`);
+            return Number(numKey);
         }
     }
-
-    if (A.length === 1) {
-        oddOneOut = A[0];
-    } else if (A.length > 1) {
-        console.error("Error: More than one unpaired value found in array.");
-    }
-
-    return oddOneOut;
 }
 
 let testArr = [9, 3, 9, 3, 9, 7, 9];
